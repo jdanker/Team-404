5aa03dee2821ad1bf7cb4a65dcc6022aa0a61af0
Thel Gunter <thelg4@hotmail.com>
2019-11-06 08:31:01 -0500

    Added ChildUser class

--- src/ChildUser.java
+++ src/ChildUser.java
@@ -1,5 +1,8 @@
 import java.util.*;
 
+/**
+ * @author Larry Gunter
+ */
 public class ChildUser extends User {
 
     private long balance;
@@ -21,7 +24,7 @@ public class ChildUser extends User {
     /**
      * Method to make a childUser a standardUser when they turn 18
      */
-    public User TransformAccount()  {
-
-    }
+//    public User TransformAccount()  {
+//
+//    }
 }
