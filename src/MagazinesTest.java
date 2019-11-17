30dbbc5d65207f8095a478688a68f3d2d3869dec
Jahred Danker <jahrede@gmail.com>
2019-11-17 17:48:35 -0500

    made testing classes

--- src/MagazinesTest.java
+++ src/MagazinesTest.java
@@ -3,12 +3,13 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MagazinesTest {
-    
+
     @org.junit.jupiter.api.Test
     void printMagazines() {
     }
 
     @org.junit.jupiter.api.Test
     void searchMagazines() {
+
     }
 }
