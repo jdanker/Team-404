d6d9989ab6cc8619e2f71767033516844bcbe3b1
Thel Gunter <thelg4@hotmail.com>
2019-11-20 16:18:35 -0500

    Added Media test

--- src/MediaTest.java
+++ src/MediaTest.java
@@ -6,18 +6,25 @@ class MediaTest {
 
     @Test
     void getID() {
-
-    }
-
-    @Test
-    void getDueDate() {
+        long id;
+        DVD dvd = new DVD(1234, "a dvd", 2000, "action", "none", "director", 3, false, "it's great");
+        id = dvd.getID();
+        assertEquals(1234, id);
     }
 
     @Test
     void getTitle() {
+        String title;
+        DVD dvd = new DVD(1234, "a dvd", 2000, "action", "none", "director", 3, false, "it's great");
+        title = dvd.getTitle();
+        assertEquals("a dvd", title);
     }
 
     @Test
     void getNumCopies() {
+        long numCopies;
+        DVD dvd = new DVD(1234, "a dvd", 2000, "action", "none", "director", 3, false, "it's great");
+        numCopies = dvd.getNumCopies();
+        assertEquals(3, numCopies);
     }
 }
